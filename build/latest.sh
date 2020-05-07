e7a8747aa8e89f4a497c83dce4d0610ff8ab42dd
Amin NAIRI <nairi.amin@gmail.com>
2020-05-07 17:51:05 +0200

    :arrow_up: upgraded the version

--- build/latest.sh
+++ build/latest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=0.42.0
+VERSION=1.0.0-rc1
 
 docker build --build-arg VERSION=$VERSION --rm --pull --no-cache --force-rm --compress --tag aminnairi/deno:latest --file ./docker/Dockerfile . \
     && docker push aminnairi/deno:latest \
