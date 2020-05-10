c11e54ad62911f5842581dc8dc22fcb7f7b95cf8
Amin NAIRI <nairi.amin@gmail.com>
2020-05-10 22:08:32 +0200

    :sparkles: added the version 1.0.0-rc2

--- build/latest.sh
+++ build/latest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.0.0-rc1
+VERSION=1.0.0-rc2
 
 docker build --build-arg VERSION=$VERSION --rm --pull --no-cache --force-rm --compress --tag aminnairi/deno:latest --file ./docker/Dockerfile . \
     && docker push aminnairi/deno:latest \
