b3826792e4bf036b7b36e3e86b1ce296d4a49697
Amin NAIRI <nairi.amin@gmail.com>
2020-05-21 12:58:35 +0200

    :building_construction: removed now unecessary build files

--- build/latest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION=1.0.0
-
-docker build --build-arg VERSION=$VERSION --rm --pull --no-cache --force-rm --compress --tag aminnairi/deno:latest --file ./docker/Dockerfile . \
-    && docker push aminnairi/deno:latest \
-    && docker rmi -f aminnairi/deno:latest
